10f37297917677f45bb264a487f382c844f36c9f
LSEANL03 <haov80530@gmail.com>
2023-03-08 00:31:04 +0700

    update code

--- BaiThucHanh0703/src/Slide74.java
+++ BaiThucHanh0703/src/Slide74.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //tinh tong cac so chan trong mang
-public class App {
+public class Slide74 {
     public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
         int n,tong=0;
